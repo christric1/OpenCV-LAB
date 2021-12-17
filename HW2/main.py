4727e3ecac08fd0f6e1373d8d4d3bc5921ddf4e8
christric1 <ra2847545015@gmail.com>
2021-12-18 01:08:36 +0800

    Create UI.

--- HW2/main.py
+++ HW2/main.py
@@ -11,7 +11,6 @@ class Main_window(QWidget):
 
         self.setWindowTitle('HW1')
         self.setStyleSheet("font: 12pt Arial")
-        self.setFixedSize(960, 300)
 
         self.hBox = QHBoxLayout(self)
 
@@ -22,9 +21,41 @@ class Main_window(QWidget):
         self.groupBox1_btn2 = QPushButton("2.2 Find Intrinsic ", self.groupBox1)
 
         self.groupBox2 = QGroupBox("Find Extrinsic")
+        self.vBox2 = QVBoxLayout(self.groupBox2)
+        self.hBox2 = QHBoxLayout(self)
+        self.label1 = QLabel("Select image : ", self.groupBox2)
+        self.lineEdit = QLineEdit()
+        self.groupBox2_btn1 = QPushButton("2.3 Find Extrinsic ", self.groupBox2)
+        self.hBox2.addWidget(self.label1, 1)
+        self.hBox2.addWidget(self.lineEdit, 1)
+        self.vBox2.addLayout(self.hBox2, 1)
+        self.vBox2.addWidget(self.groupBox2_btn1, 1)
+
+        self.groupBox1_btn3 = QPushButton("2.4 Find Distortion", self.groupBox1)
+        self.groupBox1_btn4 = QPushButton("2.5 Show Result", self.groupBox1)
 
         self.vBox1.addWidget(self.groupBox1_btn1, 1)
         self.vBox1.addWidget(self.groupBox1_btn2, 1)
+        self.vBox1.addWidget(self.groupBox2, 1)
+        self.vBox1.addWidget(self.groupBox1_btn3, 1)
+        self.vBox1.addWidget(self.groupBox1_btn4, 1)
+
+        self.groupBox3 = QGroupBox('ResNet50')
+        self.vBox3 = QVBoxLayout(self.groupBox3)
+        self.groupBox3_btn1 = QPushButton("5.1 Show model structure", self.groupBox1)
+        self.groupBox3_btn2 = QPushButton("5.2 Show tensorBoard", self.groupBox1)
+        self.groupBox3_btn3 = QPushButton("5.3 Test", self.groupBox1)
+        self.lineEdit2 = QLineEdit()
+        self.groupBox3_btn4 = QPushButton("5.4 Data Augmantation", self.groupBox1)
+
+        self.vBox3.addWidget(self.groupBox3_btn1, 1)
+        self.vBox3.addWidget(self.groupBox3_btn2, 1)
+        self.vBox3.addWidget(self.groupBox3_btn3, 1)
+        self.vBox3.addWidget(self.lineEdit2, 1)
+        self.vBox3.addWidget(self.groupBox3_btn4, 1)
+
+        self.hBox.addWidget(self.groupBox1, 1)
+        self.hBox.addWidget(self.groupBox3, 1)
 
 
 if __name__ == '__main__':
